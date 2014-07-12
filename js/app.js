72b8159fdfcfad5b1e21ab32de440e57e4d95557
jameswulfy <wulfensteinjames@gmail.com>
2014-07-12 13:40:47 -0600

    add js/app.js

--- js/app.js
+++ js/app.js
@@ -9,15 +9,10 @@ var toggleComposeHeight = function(){
 	});
 };
 
-var countChar = function(){
-	var compose = $('#tweet-content .tweet-compose');
-	var charCount = $('#char-count');
-	charCount.text(140 - compose.val().length)
-}
-
-
 $( document ).ready(function() {
+
 	$("#tweet-controls").hide();
+
 //step one capture click in text area
 $('.tweet-compose').click(function() {
 	toggleComposeHeight();
@@ -25,7 +20,28 @@ $('.tweet-compose').click(function() {
 	$('#tweet-controls').toggle();
 //step three double the textArea
 	$('.tweet-compose').height();
-})
 });
 
 $("#active-user").text("JamesWulfy");
+
+$('#tweet').keyup(function() {
+	var count = $("#tweet").val().length;
+	$('#char-count').text(140 - count);
+	if (count > 130) {
+		$('#char-count').css('color','red');
+	}
+	else {
+		$('#char-count').css('color','black');
+	}
+	if (count > 140) {
+		$("#tweet-submit").attr("disabled", "true");
+	}
+	else {
+		$('#tweet-submit').removeAttr('disabled');
+	}
+});
+
+});
+
+
+
